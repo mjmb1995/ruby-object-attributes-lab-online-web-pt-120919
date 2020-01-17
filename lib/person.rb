957f7d7aadf81343eab5a9a866440de4c611a653
Mark Bihag <markbihag330@gmail.com>
2020-01-17 18:32:30 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(this_persons_name)
+    @name = this_persons_name
+  end
+  def name
+    @name
+  end
+  def job=(this_persons_job)
+    @job = this_persons_job
+  end
+  def job
+    @job
+  end
+end
